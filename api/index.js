41e3962a03d5573f899a1f464a8f0bb268b4d61a
ibeeeees <iibrah.alii@gmail.com>
2025-10-24 11:09:48 -0400

    Remove dotenv from Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 // Vercel Serverless Function - Express API
 "use strict";
 
-require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const axios = require("axios");
